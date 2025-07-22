5f2552d6a8e3cd44d7875ab51fe5aea1e74d5597
marcos willian <mw466169@gmail.com>
2025-07-22 15:09:11 -0300

    continuar com google

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-
+import { GoogleService } from './google-auth.service';
 @Module({
+  
   imports: [JwtModule.register({
     secret: 'meu_segredo',
     signOptions: { expiresIn: '1d' } // Token expiration time
   })],
-  providers: [AuthService, PrismaService, JwtStrategy],
+  providers: [AuthService, PrismaService, JwtStrategy, GoogleService],
   controllers: [AuthController]
 })
 export class AuthModule {}
